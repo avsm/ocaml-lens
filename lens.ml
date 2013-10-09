a7c8447324b30977776f10213744d0ef36c2dfb5
Anil Madhavapeddy <anil@recoil.org>
2013-10-09 09:20:09 +0100

    Add standalone Lens, extracted from astrada/gapi repository

---
+++ lens.ml
@@ -0,0 +1,188 @@
+(*
+   Copyright (c) 2011-2012 Alessandro Strada
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*)
+
+let (|-) f g x = g (f x)
+
+type ('a, 'b) t = {
+  get : 'a -> 'b;
+  set : 'b -> 'a -> 'a
+}
+
+let modify l f a =
+  let value = l.get a in
+  let new_value = f value in
+  l.set new_value a
+
+let _get a l = l.get a
+
+let _set v a l = l.set v a
+
+let _modify f l = modify l f
+
+let compose l1 l2 = {
+  get = l2.get |- l1.get;
+  set = l1.set |- modify l2
+}
+
+let pair l1 l2 = {
+  get = (fun (a, b) -> (l1.get a, l2.get b));
+  set = (fun (a, c) (b, d) -> (l1.set a b, l2.set c d))
+}
+
+let pair3 l1 l2 l3 = {
+  get = (fun (a, b, c) -> (l1.get a, l2.get b, l3.get c));
+  set = (fun (a, c, e) (b, d, f) -> (l1.set a b, l2.set c d, l3.set e f))
+}
+
+let cond pred lt lf =
+  let choose a = if pred a then lt else lf in
+  { get = (fun a -> choose a |> _get a);
+    set = (fun b a -> choose a |> _set b a)
+  }
+
+let get_state l =
+  fun a -> _get a l, a
+
+let put_state l v =
+  fun a -> (), _set v a l
+
+let modify_state l f =
+  fun a -> (), _modify f l a
+
+let ignore = {
+  get = ignore;
+  set = (fun _ a -> a)
+}
+
+let id = {
+  get = (fun a -> a);
+  set = (fun b a -> b)
+}
+
+let first = {
+  get = fst;
+  set = (fun v a -> v, snd a)
+}
+
+let second = {
+  get = snd;
+  set = (fun v a -> fst a, v)
+}
+
+let head = {
+  get = List.hd;
+  set = (fun v xs -> v :: List.tl xs)
+}
+
+let tail = {
+  get = List.tl;
+  set = (fun v xs -> List.hd xs :: v)
+}
+
+let for_hash key = {
+  get = (fun h ->
+      try
+        Some (Hashtbl.find h key)
+      with Not_found -> None);
+  set = (fun v h ->
+      match v with
+        Some value -> Hashtbl.add h key value; h
+      | None -> Hashtbl.remove h key; h)
+}
+
+let for_assoc key = {
+  get = (fun l ->
+      try
+        Some (List.assoc key l)
+      with Not_found -> None);
+  set = (fun v l ->
+      match v with
+        Some value ->
+        let l' = List.remove_assoc key l in
+        (key, value) :: l'
+      | None -> List.remove_assoc key l)
+}
+
+let for_array i = {
+  get = (fun a -> Array.get a i);
+  set = (fun v a -> let a' = Array.copy a in Array.set a' i v; a')
+}
+
+let for_list i = {
+  get = (fun xs -> List.nth xs i);
+  set = (fun v xs ->
+      List.fold_left
+        (fun (xs', j) x ->
+           (if i = j then v::xs' else x::xs'), (j+1))
+        ([], 0)
+        xs |> fst |> List.rev)
+}
+
+let option_get = {
+  get = (function None -> raise Not_found | Some v -> v);
+  set = (fun v _ -> Some v)
+}
+
+let list_map l = {
+  get = List.map l.get;
+  set = List.map2 l.set
+}
+
+(* TODO: array_map *)
+
+let xmap f g l = {
+  get = l.get |- f;
+  set = g |- l.set
+}
+
+module Infix =
+struct
+  let (|.) = _get
+
+  let (^=) l v = fun a -> _set v a l
+
+  let (^%=) = modify
+
+  let (|--) l1 l2 = compose l2 l1
+
+  let (--|) = compose
+
+  let ( *** ) l1 l2 = pair l1 l2
+
+  let (+=) l v = _modify ((+) v) l
+
+  let (-=) l v = _modify ((-) v) l
+
+end
+
+module StateInfix =
+struct
+  let (^=!) l v = put_state l v
+
+  let (+=!) l v = modify_state l ((+) v)
+
+  let (-=!) l v = modify_state l ((-) v)
+
+  let (@=!) l v = modify_state l (fun a -> a @ v)
+
+end
+
---
+++ lens.mli
@@ -0,0 +1,162 @@
+(*
+   Copyright (c) 2011-2012 Alessandro Strada
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*)
+
+(** Functional lenses.
+
+    Based on F# implementation in {{:https://github.com/fsharp/fsharpx}FSharpx}
+    (see {{:https://github.com/fsharp/fsharpx/blob/master/src/FSharpx.Core/Lens.fs}src/FSharpx.Core/Lens.fs} for the original implementation)
+
+    @see <http://bugsquash.blogspot.com/2011/11/lenses-in-f.html> Lenses in F#
+    @see <http://stackoverflow.com/questions/8179485/updating-nested-immutable-data-structures> Stackoverflow question about Updating nested immutable data structures
+    @see <http://stackoverflow.com/questions/5767129/lenses-fclabels-data-accessor-which-library-for-structure-access-and-mutatio> Haskell libraries for structure access and mutation
+    @see <http://www.youtube.com/watch?v=efv0SQNde5Q> Functional lenses for Scala by Edward Kmett on YouTube
+    @see <http://patternsinfp.wordpress.com/2011/01/31/lenses-are-the-coalgebras-for-the-costate-comonad/> Lenses are the coalgebras for the costate comonad by Jeremy Gibbons
+*)
+
+(** Lens type definition *)
+type ('a, 'b) t = {
+  get : 'a -> 'b;
+  (** Functional getter *)
+  set : 'b -> 'a -> 'a
+  (** Functional setter *)
+}
+
+(** Updates a value through a lens *)
+val modify : ('a, 'b) t -> ('b -> 'b) -> 'a -> 'a
+
+(** {3 Combinators} *)
+
+(** Sequentially composes two lenses *)
+val compose : ('a, 'b) t -> ('c, 'a) t -> ('c, 'b) t
+
+(** Pairs two lenses *)
+val pair : ('a, 'b) t -> ('c, 'd) t -> ('a * 'c, 'b * 'd) t
+
+(** Pairs three lenses *)
+val pair3 : ('a, 'b) t -> ('c, 'd) t -> ('e, 'f) t -> ('a * 'c * 'e, 'b * 'd * 'f) t
+
+(** Selects a lens checking a predicate.
+
+    [cond pred lensTrue lensFalse]: [pred] is applied to source. If [true], [lensTrue] is selected. If [false], [lensFalse] is selected. *)
+val cond : ('a -> bool) -> ('a, 'b) t -> ('a, 'b) t -> ('a, 'b) t
+
+(** {3 State monad integration} *)
+
+(** Gets a value from the state monad. *)
+val get_state : ('a, 'b) t -> 'a -> 'b * 'a
+
+(** Puts a value in the state monad. *)
+val put_state : ('a, 'b) t -> 'b -> 'a -> unit * 'a
+
+(** Modifies a value in the state monad. *)
+val modify_state : ('a, 'b) t -> ('b -> 'b) -> 'a -> unit * 'a
+
+(** {3 Stock lenses} *)
+
+(** Trivial lens *)
+val ignore : ('a, unit) t
+
+(** Identity lens *)
+val id : ('a, 'a) t
+
+(** Gets/sets the first element in a pair *)
+val first : ('a * 'b, 'a) t
+
+(** Gets/sets the second element in a pair *)
+val second : ('a * 'b, 'b) t
+
+(** Gets/sets the first element in a list *)
+val head : ('a list, 'a) t
+
+(** Gets/sets the tail of a list *)
+val tail : ('a list, 'a list) t
+
+(** Lens for a particular key in a hashtable *)
+val for_hash : 'a -> (('a, 'b) Hashtbl.t, 'b option) t
+
+(** Lens for a particular key in an associative list *)
+val for_assoc : 'a -> (('a * 'b) list, 'b option) t
+
+(** Lens for a particular position in an array *)
+val for_array : int -> ('a array, 'a) t
+
+(** Lens for a particular position in a list *)
+val for_list : int -> ('a list, 'a) t
+
+(** Lens for extracting the value from an option type (same as Option.get) *)
+val option_get : ('a option, 'a) t
+
+(** {3 List combinators} *)
+
+(** Creates a lens that maps the given lens in a list *)
+val list_map : ('a, 'b) t -> ('a list, 'b list) t
+
+(** {3 Isomorphism} *)
+
+(** Applies an isomorphism to the value viewed through a lens *)
+val xmap : ('a -> 'b) -> ('b -> 'a) -> ('c, 'a) t -> ('c, 'b) t
+
+(** Infix operators *)
+module Infix :
+sig
+  val ( |. ) : 'a -> ('a, 'b) t -> 'b
+  (** Get operator *)
+
+  val ( ^= ) : ('a, 'b) t -> 'b -> 'a -> 'a
+  (** Set operator *)
+
+  val ( ^%= ) : ('a, 'b) t -> ('b -> 'b) -> 'a -> 'a
+  (** Mod operator *)
+
+  (** {3 Composition} *)
+
+  val ( |-- ) : ('a, 'b) t -> ('b, 'c) t -> ('a, 'c) t
+  (** Flipped compose operator *)
+
+  val ( --| ) : ('a, 'b) t -> ('c, 'a) t -> ('c, 'b) t
+  (** Compose operator *)
+
+  val ( *** ) : ('a, 'b) t -> ('c, 'd) t -> ('a * 'c, 'b * 'd) t
+  (** Pair operator *)
+
+  (** {3 Pseudo-imperatives} *)
+
+  val ( += ) : ('a, int) t -> int -> 'a -> 'a
+
+  val ( -= ) : ('a, int) t -> int -> 'a -> 'a
+
+end
+
+(** Infix operators for the state monad *)
+module StateInfix :
+sig
+  val ( ^=! ) : ('a, 'b) t -> 'b -> 'a -> unit * 'a
+  (** Set operator *)
+
+  (** {3 Pseudo-imperatives} *)
+
+  val ( +=! ) : ('a, int) t -> int -> 'a -> unit * 'a
+
+  val ( -=! ) : ('a, int) t -> int -> 'a -> unit * 'a
+
+  val ( @=! ) : ('a, 'b list) t -> 'b list -> 'a -> unit * 'a
+end
