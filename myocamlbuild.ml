bb5b2c5f848b3d908c87f3bb274d0faf6dcf4e55
Didier Wenzek <didier.wenzek@acidalie.com>
2015-07-21 10:47:51 +0200

    Regen oasis generated files with ppx_deriving package.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,5 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 2ee1b65797d78eba0e238e3eeb0c20c5) *)
+(* DO NOT EDIT (digest: dd9a90e32eacaaac7410f7309b99f4e4) *)
 module OASISGettext = struct
 (* # 22 "src/oasis/OASISGettext.ml" *)
 
@@ -607,10 +607,43 @@ end
 open Ocamlbuild_plugin;;
 let package_default =
   {
-     MyOCamlbuildBase.lib_ocaml = [("lens", [], [])];
+     MyOCamlbuildBase.lib_ocaml =
+       [("lens", [], []); ("lens_ppx_deriving", ["ppx_deriving"], [])];
      lib_c = [];
-     flags = [];
-     includes = []
+     flags =
+       [
+          (["oasis_executable_ppx_test_byte"; "ocaml"; "link"; "byte"],
+            [
+               (OASISExpr.EBool true,
+                 S
+                   [
+                      A "-ppx";
+                      A
+                        "ocamlfind ppx_deriving/ppx_deriving ppx_deriving/lens_ppx_deriving.cma"
+                   ])
+            ]);
+          (["oasis_executable_ppx_test_byte"; "ocaml"; "ocamldep"; "byte"],
+            [
+               (OASISExpr.EBool true,
+                 S
+                   [
+                      A "-ppx";
+                      A
+                        "ocamlfind ppx_deriving/ppx_deriving ppx_deriving/lens_ppx_deriving.cma"
+                   ])
+            ]);
+          (["oasis_executable_ppx_test_byte"; "ocaml"; "compile"; "byte"],
+            [
+               (OASISExpr.EBool true,
+                 S
+                   [
+                      A "-ppx";
+                      A
+                        "ocamlfind ppx_deriving/ppx_deriving ppx_deriving/lens_ppx_deriving.cma"
+                   ])
+            ])
+       ];
+     includes = [("ppx_test", ["ppx_deriving"])]
   }
   ;;
 
@@ -618,6 +651,6 @@ let conf = {MyOCamlbuildFindlib.no_automatic_syntax = false}
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default conf package_default;;
 
-# 622 "myocamlbuild.ml"
+# 655 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
